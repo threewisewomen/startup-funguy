efc92167b37794a4f38e87f19d71b6d3eae02593
John Mathew <jmathew@acilogistix.com>
2024-12-15 18:20:51 +0530

    project directory setup and dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ReactDom from 'react-dom/client'
+ 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Router>
     <App />
+    </Router>
   </StrictMode>,
 )
