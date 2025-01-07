f23b1ef97a7911ccd988283eeced8e0be52245d3
John Mathew <jmathew@acilogistix.com>
2025-01-07 18:57:06 +0530

    Header section with Hamburger menu

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ( { className, href, onClick, children, px, white}) => {
     const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8': 'text-n-1'} ${className || ''}` ;
     const spanClasses = "relative z-10";
     const renderButton = () => (
-        <button className={classes}>
+        <button className={classes} onClick={onClick} >
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
 
@@ -12,7 +12,7 @@ const Button = ( { className, href, onClick, children, px, white}) => {
     );
 
     const renderLink = () => (
-        <a href={href} className={classes}>
+        <a href={href} className={classes} >
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </a>
